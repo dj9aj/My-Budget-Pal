1418735c3fae83ac0e9c25bc2ca7f59ea5132de8
Jack Doyle <jackdoyle@Jacks-MacBook-Pro.local>
2018-05-16 18:47:15 +0100

    Add mobile compatibility

--- routes/index.js
+++ routes/index.js
@@ -36,8 +36,7 @@ router.post("/register", (req, res, next) => {
     const newUser = new User({username: req.body.username}); 
     User.register(newUser, req.body.password, (err, user) => {
         if(err) {
-            console.log(err);
-            return res.render("register");
+            return res.render("register", {error: err.message}); 
         }
         passport.authenticate("local")(req, res, () => {
             res.redirect("/budget/" + user.username);
